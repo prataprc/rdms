05d4f70a0609fe02a3c732dd89a09b4237672655
prataprc <prataprc@gmail.com>
2019-05-28 19:44:21 +0530

    cleanup and corner case fixes.

--- src/util.rs
+++ src/util.rs
@@ -3,21 +3,25 @@ use std::{fs, path};
 use crate::core::Result;
 use crate::error::BognError;
 
+// open file for writing, if append is false, create file.
 pub(crate) fn open_file_w(file: &str, append: bool) -> Result<fs::File> {
-    let p = path::Path::new(&file);
-
-    let parent = p.parent().ok_or(BognError::InvalidFile(file.to_string()))?;
-    fs::create_dir_all(parent)?;
+    let p = path::Path::new(file);
 
     let mut opts = fs::OpenOptions::new();
     Ok(match append {
-        false => opts.append(true).create_new(true).open(p)?,
+        false => {
+            let err = BognError::InvalidFile(file.to_string());
+            let parent = p.parent().ok_or(err)?;
+            fs::create_dir_all(parent)?;
+            opts.append(true).create_new(true).open(p)?
+        }
         true => opts.append(true).open(p)?,
     })
 }
 
+// open file for reading.
 pub(crate) fn open_file_r(file: &str) -> Result<fs::File> {
-    let p = path::Path::new(&file);
+    let p = path::Path::new(file);
     let mut opts = fs::OpenOptions::new();
     Ok(opts.read(true).create_new(true).open(p)?)
 }
--- src/vlog.rs
+++ src/vlog.rs
@@ -12,25 +12,32 @@ use crate::error::BognError;
 // |                 payload                  |
 // *-------------------*----------------------*
 //
-// bit 60 shall be set.
+// Flags:
+// * bit 60 shall be set.
+// * bit 61 reserved
+// * bit 62 reserved
+// * bit 63 reserved
 
 #[derive(Clone)]
 pub(crate) enum Value<V>
 where
     V: Default + Serialize,
 {
+    // Native value, already de-serialized.
     Native {
         value: V,
     },
+    // Refers to serialized value on disk, either index-file or vlog-file
     Reference {
-        fpos: u64, // point to value payload in index-file or vlog-file
+        fpos: u64,
         length: u64,
     },
+    // Refers to serialized value on disk, either index-file or vlog-file.
     Backup {
-        file: String, // must be a vlog or index filename with path
-        fpos: u64,    // point to payload in index-file or vlog-file
+        file: String,
+        fpos: u64,
         length: u64,
-    }, // points to entry on disk.
+    },
 }
 
 impl<V> Value<V>
@@ -47,70 +54,11 @@ where
         Value::Reference { fpos, length }
     }
 
-    pub(crate) fn new_backup(file: String, fpos: u64, length: u64) -> Value<V> {
-        Value::Backup { file, fpos, length }
-    }
-
-    pub(crate) fn is_reference(&self) -> bool {
-        match self {
-            Value::Reference { .. } => true,
-            Value::Native { .. } | Value::Backup { .. } => false,
-        }
-    }
-
-    pub(crate) fn to_native(self, fd: Option<&mut fs::File>) -> Result<Value<V>> {
-        match (self, fd) {
-            (obj @ Value::Native { .. }, _) => Ok(obj),
-            (Value::Reference { fpos, length }, Some(fd)) => Self::read(fd, fpos, length as usize),
-            (Value::Reference { .. }, None) => panic!("invalid call !!"),
-            (Value::Backup { fpos, length, .. }, Some(fd)) => Self::read(fd, fpos, length as usize),
-            (Value::Backup { file, fpos, length }, None) => {
-                let mut fd = fs::OpenOptions::new().read(true).open(file)?;
-                Self::read(&mut fd, fpos, length as usize)
-            }
-        }
-    }
-
-    pub(crate) fn flush(self, fd: &mut fs::File, buf: &mut Vec<u8>) -> Result<Value<V>> {
-        match self {
-            Value::Native { value } => Self::append(value, fd, buf),
-            obj @ Value::Reference { .. } => Ok(obj),
-            Value::Backup { .. } => panic!("impossible situation"),
-        }
-    }
-
-    fn read(fd: &mut fs::File, fpos: u64, ln: usize) -> Result<Value<V>> {
-        let mut buf = Vec::with_capacity(ln);
-        buf.resize(buf.capacity(), 0);
-        fd.seek(io::SeekFrom::Start(fpos + 8))?;
-        let n = fd.read(&mut buf)?;
-        if n == ln {
-            let mut value: V = Default::default();
-            value.decode(&buf)?;
-            Ok(Value::Native { value })
-        } else {
-            Err(BognError::PartialRead(ln, n))
-        }
-    }
-
-    fn append(value: V, fd: &mut fs::File, buf: &mut Vec<u8>) -> Result<Value<V>> {
-        let fpos = fd.metadata()?.len();
-        buf.resize(0, 0);
-        value.encode(buf);
-        let length = buf.len();
-        let scratch = ((length as u64) | Self::VALUE_FLAG).to_be_bytes();
-        let total_len = length + scratch.len();
-
-        // TODO: can we avoid 2 writes ?
-        let mut n = fd.write(&scratch)?;
-        n += fd.write(buf)?;
-        if n != total_len {
-            Err(BognError::PartialWrite(total_len, n))
-        } else {
-            Ok(Value::Reference {
-                fpos,
-                length: length as u64,
-            })
+    pub(crate) fn new_backup(file: &str, fpos: u64, length: u64) -> Value<V> {
+        Value::Backup {
+            file: file.to_string(),
+            fpos,
+            length,
         }
     }
 }
@@ -121,25 +69,32 @@ where
 // |                 payload                  |
 // *-------------------*----------------------*
 //
-// bit 60 shall be clear.
+// Flags:
+// * bit 60 shall be clear.
+// * bit 61 reserved
+// * bit 62 reserved
+// * bit 63 reserved
 
 #[derive(Clone)]
 pub(crate) enum Delta<V>
 where
     V: Default + Diff,
 {
+    // Native delta, already de-serialized.
     Native {
         delta: <V as Diff>::D,
     },
+    // Refers to serialized delta on disk, either index-file or vlog-file
     Reference {
-        fpos: u64, // point to value payload in vlog-file
+        fpos: u64,
         length: u64,
     },
+    // Refers to serialized value on disk, either index-file or vlog-file.
     Backup {
-        file: String, // must be a vlog file name, with full path
-        fpos: u64,    // point to value payload in vlog-file
+        file: String,
+        fpos: u64,
         length: u64,
-    }, // points to entry on disk.
+    },
 }
 
 impl<V> Delta<V>
@@ -156,88 +111,5 @@ where
 
     pub(crate) fn new_backup(file: String, fpos: u64, length: u64) -> Delta<V> {
         Delta::Backup { file, fpos, length }
-    }
-
-    pub fn append_to(
-        self,
-        fd: &mut fs::File,
-        buf: &mut Vec<u8>, /* reusable buffer*/
-    ) -> Result<Delta<V>> {
-        match self {
-            Delta::Native { delta } => {
-                let fpos = fd.metadata()?.len();
-                buf.resize(0, 0);
-                delta.encode(buf);
-                let length = buf.len() as u64;
-                let scratch = length.to_be_bytes();
-
-                let total_len = length + (scratch.len() as u64);
-                let mut n = fd.write(&scratch)?;
-                n += fd.write(buf)?;
-                if (n as u64) != total_len {
-                    Err(BognError::PartialWrite(total_len as usize, n))
-                } else {
-                    Ok(Delta::Reference { fpos, length })
-                }
-            }
-            obj @ Delta::Reference { .. } => Ok(obj),
-            Delta::Backup { .. } => panic!("impossible situation"),
-        }
-    }
-
-    pub(crate) fn to_native(self, fd: Option<&mut fs::File>) -> Result<Delta<V>> {
-        match (self, fd) {
-            (obj @ Delta::Native { .. }, _) => Ok(obj),
-            (Delta::Reference { fpos, length }, Some(fd)) => Self::read(fd, fpos, length as usize),
-            (Delta::Reference { .. }, None) => panic!("invalid call !!"),
-            (Delta::Backup { fpos, length, .. }, Some(fd)) => Self::read(fd, fpos, length as usize),
-            (Delta::Backup { file, fpos, length }, None) => {
-                let mut fd = fs::OpenOptions::new().read(true).open(file)?;
-                Self::read(&mut fd, fpos, length as usize)
-            }
-        }
-    }
-
-    pub(crate) fn flush(self, fd: &mut fs::File, buf: &mut Vec<u8>) -> Result<Delta<V>> {
-        match self {
-            Delta::Native { delta } => Self::append(delta, fd, buf),
-            obj @ Delta::Reference { .. } => Ok(obj),
-            Delta::Backup { .. } => panic!("impossible situation"),
-        }
-    }
-
-    fn read(fd: &mut fs::File, fpos: u64, ln: usize) -> Result<Delta<V>> {
-        let mut buf = Vec::with_capacity(ln);
-        buf.resize(buf.capacity(), 0);
-        fd.seek(io::SeekFrom::Start(fpos + 8))?;
-        let n = fd.read(&mut buf)?;
-        if n == buf.len() {
-            let mut delta: <V as Diff>::D = Default::default();
-            delta.decode(&buf)?;
-            Ok(Delta::Native { delta })
-        } else {
-            Err(BognError::PartialRead(buf.len(), n))
-        }
-    }
-
-    fn append(delta: <V as Diff>::D, fd: &mut fs::File, buf: &mut Vec<u8>) -> Result<Delta<V>> {
-        let fpos = fd.metadata()?.len();
-        buf.resize(0, 0);
-        delta.encode(buf);
-        let length = buf.len();
-        let scratch = (length as u64).to_be_bytes();
-        let total_len = length + scratch.len();
-
-        // TODO: can we avoid 2 writes ?
-        let mut n = fd.write(&scratch)?;
-        n += fd.write(buf)?;
-        if n != total_len {
-            Err(BognError::PartialWrite(total_len, n))
-        } else {
-            Ok(Delta::Reference {
-                fpos,
-                length: length as u64,
-            })
-        }
     }
 }

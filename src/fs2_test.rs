33b3b7dbd7283252cad8662c17f9129af903bde4
prataprc <prataprc@gmail.com>
2019-10-18 20:02:55 +0530

    file_lock: unit test cases.

--- src/fs2_test.rs
+++ src/fs2_test.rs
@@ -3,7 +3,7 @@ use fs2::FileExt;
 use std::{fs, io};
 
 #[test]
-fn test_write_lock() {
+fn test_file_lock() {
     let file = {
         let mut dir = std::env::temp_dir();
         dir.push("fs2-test-write-lock.data");
@@ -12,6 +12,7 @@ fn test_write_lock() {
     println!("lock experiment with file {:?}", file);
     fs::remove_file(&file).ok();
 
+    // shared lock 1
     let fd1 = {
         let mut opts = fs::OpenOptions::new();
         opts.append(true)
@@ -21,6 +22,7 @@ fn test_write_lock() {
     };
     fd1.lock_shared().unwrap();
 
+    // shared lock 2
     let fd2 = {
         let mut opts = fs::OpenOptions::new();
         opts.write(true).open(file.clone()).unwrap()
@@ -30,18 +32,39 @@ fn test_write_lock() {
         Err(err) if err.kind() == io::ErrorKind::WouldBlock => (),
         Err(err) => panic!("unexpected err: {:?}", err),
     }
-
     fd2.lock_shared().unwrap();
 
+    // shared lock 3
+    let fd3 = {
+        let mut opts = fs::OpenOptions::new();
+        opts.write(true).open(file.clone()).unwrap()
+    };
+    match fd3.try_lock_exclusive() {
+        Ok(_) => panic!("unexpected behaviour!!"),
+        Err(err) if err.kind() == io::ErrorKind::WouldBlock => (),
+        Err(err) => panic!("unexpected err: {:?}", err),
+    }
+
+    // unlock as shared lock 2
     fd2.unlock();
     match fd2.try_lock_exclusive() {
         Ok(_) => panic!("unexpected behaviour!!"),
         Err(err) if err.kind() == io::ErrorKind::WouldBlock => (),
         Err(err) => panic!("unexpected err: {:?}", err),
     }
+    match fd3.try_lock_exclusive() {
+        Ok(_) => panic!("unexpected behaviour!!"),
+        Err(err) if err.kind() == io::ErrorKind::WouldBlock => (),
+        Err(err) => panic!("unexpected err: {:?}", err),
+    }
 
+    // unlock shared lock 1
     fd1.unlock();
-
+    match fd3.try_lock_exclusive() {
+        Ok(_) => (),
+        Err(err) => panic!("unexpected err: {:?}", err),
+    }
+    fd3.unlock();
     match fd2.try_lock_exclusive() {
         Ok(_) => (),
         Err(err) => panic!("unexpected err: {:?}", err),
@@ -57,6 +80,5 @@ fn test_write_lock() {
         Err(err) => panic!("unexpected err: {:?}", err),
     }
 
-    std::thread::sleep(std::time::Duration::new(1000, 0));
     fd2.unlock();
 }

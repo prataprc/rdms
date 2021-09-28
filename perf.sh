54ea798073793b9b228aef38fda266b33be85ffa
prataprc <prataprc@gmail.com>
2021-09-28 22:18:46 +0530

    package management: perf.sh

---
+++ perf.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env bash
+
+exec > $1
+exec 2>&1
+
+set -o xtrace
+
+# regular benchmark
+# date; time cargo +nightly bench -- --nocapture || exit $?
+# TODO: date; time cargo +stable bench -- --nocapture || exit $?
+
+# invoke perf binary
+date; time cargo +nightly run --release --bin rdms-perf --features=rdms-perf -- --profile perf-profiles/default-llrb.toml llrb
+# invoke perf binary, with valgrid
+date; valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes cargo +nightly run --release --bin rdms-perf --features=rdms-perf -- --profile perf-profiles/default-llrb.toml llrb

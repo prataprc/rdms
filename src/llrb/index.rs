ef4b6edb618dcbb892c896f393f8e87f9d600108
prataprc <prataprc@gmail.com>
2021-10-05 20:47:18 +0530

    llrb: fix load_index() for test cases.

--- src/llrb/index.rs
+++ src/llrb/index.rs
@@ -1663,7 +1663,7 @@ pub fn load_index<K>(
     seqno: Option<u64>,
 ) -> Index<K, u64>
 where
-    K: Clone + Ord + db::Footprint,
+    K: Clone + Ord + db::Footprint + fmt::Debug,
     rand::distributions::Standard: rand::distributions::Distribution<K>,
 {
     let mut rng = SmallRng::from_seed(seed.to_le_bytes());
@@ -1674,7 +1674,12 @@ where
     while (se + it + ds + rs) > 0 {
         let key: K = rng.gen();
         let value: u64 = rng.gen::<u64>();
-        // println!("{} seqno:{} {}", (se + it + ds + rs), key, index.to_seqno() + 1,);
+        //println!(
+        //    "load index {} key:{:?} seqno:{}",
+        //    (se + it + ds + rs),
+        //    key,
+        //    index.to_seqno() + 1,
+        //);
         match rng.gen::<u64>() % (se + it + ds + rs) {
             k if k < se => {
                 index.set(key, value).ok();
@@ -1684,17 +1689,14 @@ where
                 index.insert(key, value).ok();
                 it -= 1;
             }
-            k => match index.get(&key) {
-                Ok(entry) if !entry.is_deleted() && (k < (se + it + ds)) => {
-                    index.delete(&key).unwrap();
-                    ds -= 1;
-                }
-                Ok(entry) if !entry.is_deleted() => {
-                    index.remove(&key).unwrap();
-                    rs -= 1;
-                }
-                _ => (),
-            },
+            k if (k < (se + it + ds)) => {
+                index.delete(&key).unwrap();
+                ds -= 1;
+            }
+            _ => {
+                index.remove(&key).unwrap();
+                rs -= 1;
+            }
         }
     }
 

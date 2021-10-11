04a94f3bff6d5a8266ecff8692c7a3791e6c48d0
prataprc <prataprc@gmail.com>
2021-10-11 19:53:36 +0530

    types: Binary implement Cborize, ToString, Arbitrary and random

--- src/db/types.rs
+++ src/db/types.rs
@@ -1,6 +1,7 @@
 //! Implement Diff, Footprint traits for native types and std-types.
 
-use std::convert::TryFrom;
+use cbordata::Cborize;
+use std::{convert::TryFrom, hash::Hash};
 
 use crate::{
     db::{Diff, Footprint},
@@ -77,26 +78,67 @@ impl Footprint for String {
     }
 }
 
-#[derive(Clone, Debug, PartialEq, PartialOrd, Eq, Ord)]
-pub struct Binary(pub Vec<u8>);
+const BINARY_VER: u32 = 0x00170001_u32;
+
+#[derive(Clone, Default, Debug, PartialEq, PartialOrd, Eq, Ord, Hash, Cborize)]
+pub struct Binary {
+    pub val: Vec<u8>,
+}
+
+impl Binary {
+    const ID: u32 = BINARY_VER;
+}
+
+impl ToString for Binary {
+    fn to_string(&self) -> String {
+        std::str::from_utf8(&self.val).unwrap().to_string()
+    }
+}
 
 impl Diff for Binary {
     type Delta = Self;
 
     fn diff(&self, old: &Self) -> Self::Delta {
-        Binary(old.0.to_vec())
+        Binary {
+            val: old.val.to_vec(),
+        }
     }
 
     fn merge(&self, delta: &Self::Delta) -> Self {
-        Binary(delta.0.to_vec())
+        Binary {
+            val: delta.val.to_vec(),
+        }
     }
 }
 
 impl Footprint for Binary {
     fn footprint(&self) -> Result<isize> {
         use std::mem::size_of;
-        let size = size_of::<Binary>() + self.0.capacity();
+        let size = size_of::<Binary>() + self.val.capacity();
         err_at!(FailConvert, isize::try_from(size))
+    }
+}
+
+impl<'a> arbitrary::Arbitrary<'a> for Binary {
+    fn arbitrary(u: &mut arbitrary::Unstructured) -> arbitrary::Result<Self> {
+        let size = u.arbitrary::<usize>()? % 1024;
+        let val = match u.arbitrary::<u64>()? {
+            0 => Binary::default(),
+            val => Binary {
+                val: format!("{:0width$}", val, width = size).as_bytes().to_vec(),
+            },
+        };
+        Ok(val)
+    }
+}
+
+#[cfg(any(test, feature = "rand"))]
+impl rand::distributions::Distribution<Binary> for rand::distributions::Standard {
+    fn sample<R: rand::Rng + ?Sized>(&self, rng: &mut R) -> Binary {
+        let (val, size) = (rng.gen::<u64>(), rng.gen::<usize>() % 1024);
+        Binary {
+            val: format!("{:0width$}", val, width = size).as_bytes().to_vec(),
+        }
     }
 }
 

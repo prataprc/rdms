96654865e5ba17a36d9c038ecb2090580a1a9213
prataprc <prataprc@gmail.com>
2021-11-17 22:25:14 +0530

    dba: refactor files.

--- src/dba/mod.rs
+++ src/dba/mod.rs
@@ -4,10 +4,23 @@
 use crate::Result;
 
 mod entry;
+mod git;
 mod types;
 
 pub use entry::{Edge, Entry, Object, Oid, Type, User};
 
 pub trait AsKey {
     fn to_key_path(&self) -> Result<Vec<String>>;
+}
+
+pub fn to_content_key(s: &str, levels: usize) -> Option<String> {
+    match s.len() {
+        0 => None,
+        _ => {
+            let mut ss: Vec<String> =
+                s.chars().take(levels).map(|ch| ch.to_string()).collect();
+            ss.push(s.to_string());
+            Some(ss.join("/"))
+        }
+    }
 }
